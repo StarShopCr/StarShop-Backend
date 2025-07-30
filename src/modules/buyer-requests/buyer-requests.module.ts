f16c7e02d21b770f5c6fcaad0c0f734011e63cb6
clintjeff2 <clintjeff2@gmail.com>
2025-07-30 21:01:01 +0100

    feat: add User entity to buyer-requests module

--- src/modules/buyer-requests/buyer-requests.module.ts
+++ src/modules/buyer-requests/buyer-requests.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common"
-import { TypeOrmModule } from "@nestjs/typeorm"
-import { BuyerRequestsService } from "./services/buyer-requests.service"
-import { BuyerRequestsController } from "./controllers/buyer-requests.controller"
-import { BuyerRequest } from "./entities/buyer-request.entity"
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BuyerRequestsService } from './services/buyer-requests.service';
+import { BuyerRequestsController } from './controllers/buyer-requests.controller';
+import { BuyerRequest } from './entities/buyer-request.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BuyerRequest])],
+  imports: [TypeOrmModule.forFeature([BuyerRequest, User])],
   controllers: [BuyerRequestsController],
   providers: [BuyerRequestsService],
   exports: [BuyerRequestsService],
