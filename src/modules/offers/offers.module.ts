ea4709b280425fdbbfb4205476f121af4e9cd24b
CristhianRivas <c.rivas@estudiantec.cr>
2025-08-20 00:15:36 -0600

    feat: configure OffersModule with ScheduleModule and new services

--- src/modules/offers/offers.module.ts
+++ src/modules/offers/offers.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
+import { ScheduleModule } from "@nestjs/schedule"
 import { OffersService } from "./services/offers.service"
+import { OfferService } from "./services/offer.service"
 import { OfferAttachmentService } from "./services/offer-attachment.service"
+import { OfferExpirationService } from "./services/offer-expiration.service"
 import { OffersController } from "./controllers/offers.controller"
 import { Offer } from "./entities/offer.entity"
 import { OfferAttachment } from "./entities/offer-attachment.entity"
@@ -9,9 +12,13 @@ import { BuyerRequest } from "../buyer-requests/entities/buyer-request.entity"
 import { FilesModule } from "../files/files.module"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Offer, OfferAttachment, BuyerRequest]), FilesModule],
+  imports: [
+    TypeOrmModule.forFeature([Offer, OfferAttachment, BuyerRequest]), 
+    FilesModule,
+    ScheduleModule.forRoot()
+  ],
   controllers: [OffersController],
-  providers: [OffersService, OfferAttachmentService],
-  exports: [OffersService, OfferAttachmentService],
+  providers: [OffersService, OfferService, OfferAttachmentService, OfferExpirationService],
+  exports: [OffersService, OfferService, OfferAttachmentService, OfferExpirationService],
 })
 export class OffersModule {}
